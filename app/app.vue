ebb5d0a625dac41f29bc0ddaca33243b13ee618b
Davi Asaf <daviasaf16@gmail.com>
2026-06-07 10:23:29 -0300

    feat: add favicon and update app layout with head configuration

--- app/app.vue
+++ app/app.vue
@@ -1,1 +1,15 @@
-<template><UApp><NuxtLayout><NuxtPage /></NuxtLayout></UApp></template>
+<script setup lang="ts">
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+  ]
+})
+</script>
+
+<template>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
